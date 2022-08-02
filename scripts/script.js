1d2af2f3254365e70b7f622bc34bfcc9848e4b2b
Cesar Villalobos <villaloboscesar@gmail.com>
2022-08-02 18:01:42 -0400

    segunda seeccion js completada sprint_4

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 let editprofile = document.querySelector(".profile__button-edit");
 let openform = document.querySelector(".popup_opened");
 let closeform = document.querySelector(".popup__close")
+let captureName = document.querySelector(".profile__name");
+let captureAbout = document.querySelector(".profile__about");
+let setPopupName= document.querySelector(".popup__name");
+let setPopupAbout= document.querySelector(".popup__about-me")
+
 
 function openPopup (){
     openform.setAttribute("style", "display: flex");
@@ -10,5 +15,9 @@ function ClosePopup (event){
     openform.setAttribute("style", "display: none");
     event.preventDefault();
 }
+
 editprofile.addEventListener("click", openPopup);
 closeform.addEventListener("click", ClosePopup);
+
+setPopupName.setAttribute("value" , captureName.textContent);
+setPopupAbout.setAttribute("value" , captureAbout.textContent);
