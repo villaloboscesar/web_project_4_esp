1bbf3ff73aef1443cf14a383f0a2d49c3516753b
Cesar Villalobos <villaloboscesar@gmail.com>
2022-08-04 11:59:45 -0400

    funcion handler ok

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,16 @@ let openform = document.querySelector(".popup_opened");
 let closeform = document.querySelector(".popup__close")
 let captureName = document.querySelector(".profile__name");
 let captureAbout = document.querySelector(".profile__about");
-let setPopupName= document.querySelector(".popup__name");
-let setPopupAbout= document.querySelector(".popup__about-me")
+let setPopupName = document.querySelector(".popup__name");
+let setPopupAbout= document.querySelector(".popup__about-me");
 
 
 function openPopup (){
     openform.setAttribute("style", "display: flex");
 }
-
 function ClosePopup (event){
-    openform.setAttribute("style", "display: none");
-    event.preventDefault();
+   event.preventDefault();
+   openform.setAttribute("style", "display: none");
 }
 
 editprofile.addEventListener("click", openPopup);
@@ -21,3 +20,21 @@ closeform.addEventListener("click", ClosePopup);
 
 setPopupName.setAttribute("value" , captureName.textContent);
 setPopupAbout.setAttribute("value" , captureAbout.textContent);
+
+
+let form = document.querySelector(".popup");
+
+
+function handleProfileFormSubmit(evt) {
+
+    evt.preventDefault();
+    let nameinput = document.querySelector(".popup__name").value;
+    let aboutinput = document.querySelector(".popup__about-me").value;
+    captureName.textContent = nameinput;
+    captureAbout.textContent = aboutinput;
+
+}
+
+form.addEventListener('submit', handleProfileFormSubmit); 
+
+
